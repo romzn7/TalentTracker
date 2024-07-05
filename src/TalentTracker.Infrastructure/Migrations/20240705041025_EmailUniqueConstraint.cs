920292b1188cba8fca90bcfffd71d6b73e39c23e
rabbz123 <rabbz123@gmail.com>
2024-07-05 09:56:03 +0545

    Unique constraint added and migration added too

---
+++ src/TalentTracker.Infrastructure/Migrations/20240705041025_EmailUniqueConstraint.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TalentTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmailUniqueConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                schema: "tt",
+                table: "Candidates",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidates_Email",
+                schema: "tt",
+                table: "Candidates",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Candidates_Email",
+                schema: "tt",
+                table: "Candidates");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                schema: "tt",
+                table: "Candidates",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
